d7142344a5e08e52f23627980ab7abb5ac220aee
KelS2804 <ndson2804@gmail.com>
2022-07-27 14:26:42 +0700

    responsive

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,14 +2,24 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 import 'firebase/compat/storage'
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCmoFsPOWBnO0NsWQ65YosfxJTB7CIqZSo",
+//   authDomain: "onechain-c0e66.firebaseapp.com",
+//   projectId: "onechain-c0e66",
+//   storageBucket: "onechain-c0e66.appspot.com",
+//   messagingSenderId: "777431754707",
+//   appId: "1:777431754707:web:d8be21e2c5a7f6619f3126",
+//   measurementId: "G-BJZ0PJ6LDW"
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCmoFsPOWBnO0NsWQ65YosfxJTB7CIqZSo",
-  authDomain: "onechain-c0e66.firebaseapp.com",
-  projectId: "onechain-c0e66",
-  storageBucket: "onechain-c0e66.appspot.com",
-  messagingSenderId: "777431754707",
-  appId: "1:777431754707:web:d8be21e2c5a7f6619f3126",
-  measurementId: "G-BJZ0PJ6LDW"
+  apiKey: "AIzaSyBuxSgDJpgZlbclGMnw3WEoxVx7slrNm3Q",
+  authDomain: "qlsvonechain.firebaseapp.com",
+  projectId: "qlsvonechain",
+  storageBucket: "qlsvonechain.appspot.com",
+  messagingSenderId: "395102729332",
+  appId: "1:395102729332:web:e7cb8630a4d130c4e78d21",
+  measurementId: "G-QG68VTW1LY"
 };
 
 firebase.initializeApp(firebaseConfig);
